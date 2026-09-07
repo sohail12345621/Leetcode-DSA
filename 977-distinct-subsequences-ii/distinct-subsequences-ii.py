6ac0cac521b79e8901736557ba46a43c6a4ef1c0
Sohail Shaikh <504sohail@gmail.com>
2026-09-07 16:56:05 +0530

    Time: 3 ms (99.48%) | Memory: 19.3 MB (72.40%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        MOD = 10**9 + 7
+
+        dp = 1
+        last = [0] * 26
+
+        for ch in s:
+            index = ord(ch) - ord('a')
+
+            new_dp = 2 * dp - last[index]
+
+            last[index] = dp
+            dp = new_dp % MOD
+
+        return (dp - 1) % MOD
